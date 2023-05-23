f669ec4b0e23a6b32dba242d0d42aad6ba8d81ff
Shilei Liu <liushilei.wk@gmail.com>
2023-05-23 20:42:57 +0800

    Suppress warning

--- FunctionAppDotNet7Isolated/Function1.cs
+++ FunctionAppDotNet7Isolated/Function1.cs
@@ -30,7 +30,9 @@ namespace FunctionAppDotNet7Isolated
 
     public class MyInfo
     {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public MyScheduleStatus ScheduleStatus { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
         public bool IsPastDue { get; set; }
     }
