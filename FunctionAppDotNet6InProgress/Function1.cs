e39b5886445771f6013b7d091ee3250926f3914e
Shilei Liu <shileiliu@microsoft.com>
2023-05-23 14:44:45 +0800

    Add project files.

---
+++ FunctionAppDotNet6InProgress/Function1.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionAppDotNet6InProgress
+{
+    public class Function1
+    {
+        [FunctionName("Function1")]
+        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
